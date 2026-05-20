4cf2de7d651c767032ff50696b43baf0926ac6c3
Natalia Monteiro <monteiron414@gmail.com>
2026-05-20 10:53:17 -0300

    feat: adicionar classe Gato estendendo Animal com suporte a ambiente especial

--- src/model/Gato.java
+++ src/model/Gato.java
@@ -1,5 +1,31 @@
 package model;
-
-public class Gato {
-    
+ 
+public class Gato extends Animal {
+ 
+    private boolean ambienteEspecial;
+ 
+    public Gato(String nome, String porte, int idade, boolean ambienteEspecial) {
+        super(nome, porte, idade);
+        this.ambienteEspecial = ambienteEspecial;
+    }
+ 
+    @Override
+    public double calcularDiaria() {
+        return 40.0; // base fixa para gatos
+    }
+ 
+    public double calcularTotal() {
+        return calcularDiaria();
+    }
+ 
+    // Getters e Setters
+    public boolean isAmbienteEspecial() { return ambienteEspecial; }
+    public void setAmbienteEspecial(boolean ambienteEspecial) { this.ambienteEspecial = ambienteEspecial; }
+ 
+    @Override
+    public String toString() {
+        return "Gato [nome=" + getNome() + ", porte=" + getPorte() +
+               ", idade=" + getIdade() + ", ambienteEspecial=" + ambienteEspecial + "]";
+    }
 }
+ 
