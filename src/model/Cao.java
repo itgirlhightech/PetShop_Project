ccb5cced80438052fb0cc651a8a6e3167ef88e4e
Natalia Monteiro <monteiron414@gmail.com>
2026-05-20 10:50:59 -0300

    feat: adicionar classe Cao estendendo Animal com regras de passeio e diária

--- src/model/Cao.java
+++ src/model/Cao.java
@@ -1,5 +1,41 @@
 package model;
-
-public class Cao {
-    
+ 
+public class Cao extends Animal {
+ 
+    private boolean incluiPasseio;
+ 
+    public Cao(String nome, String porte, int idade, boolean incluiPasseio) {
+        super(nome, porte, idade);
+        this.incluiPasseio = incluiPasseio;
+    }
+ 
+    @Override
+    public double calcularDiaria() {
+        double diaria = 50.0; // base
+ 
+        if (getPorte().equalsIgnoreCase("grande")) {
+            diaria += 20.0;
+        }
+ 
+        return diaria;
+    }
+ 
+    public double adicionarPasseio() {
+        return incluiPasseio ? 30.0 : 0.0;
+    }
+ 
+    public double calcularTotal() {
+        return calcularDiaria() + adicionarPasseio();
+    }
+ 
+    // Getters e Setters
+    public boolean isIncluiPasseio() { return incluiPasseio; }
+    public void setIncluiPasseio(boolean incluiPasseio) { this.incluiPasseio = incluiPasseio; }
+ 
+    @Override
+    public String toString() {
+        return "Cao [nome=" + getNome() + ", porte=" + getPorte() +
+               ", idade=" + getIdade() + ", incluiPasseio=" + incluiPasseio + "]";
+    }
 }
+ 
