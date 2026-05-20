e540f5a0868736114d492d0c471ac19dacf3cd6e
Natalia Monteiro <monteiron414@gmail.com>
2026-05-20 10:45:50 -0300

    feat: add classe abstrata Animal com atributos básicos e método CalcularDiária

--- src/model/Animal.java
+++ src/model/Animal.java
@@ -1,5 +1,32 @@
 package model;
-
-public class Animal {
-    
+ 
+public abstract class Animal {
+ 
+    private String nome;
+    private String porte; // pequeno, medio, grande
+    private int idade;
+ 
+    public Animal(String nome, String porte, int idade) {
+        this.nome = nome;
+        this.porte = porte;
+        this.idade = idade;
+    }
+ 
+    public abstract double calcularDiaria();
+ 
+    // Getters e Setters
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+ 
+    public String getPorte() { return porte; }
+    public void setPorte(String porte) { this.porte = porte; }
+ 
+    public int getIdade() { return idade; }
+    public void setIdade(int idade) { this.idade = idade; }
+ 
+    @Override
+    public String toString() {
+        return "Animal [nome=" + nome + ", porte=" + porte + ", idade=" + idade + "]";
+    }
 }
+ 
